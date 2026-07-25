ad9fedac13cf4252b44b3b285f9180311c8d3ffb
ProperHW <chohw37@gmail.com>
2026-07-26 00:33:52 +0900

    flyway remove

--- backend/src/main/java/kr/co/tobetheone/ncms/NcmsApplication.java
+++ backend/src/main/java/kr/co/tobetheone/ncms/NcmsApplication.java
@@ -2,8 +2,9 @@ package kr.co.tobetheone.ncms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
 public class NcmsApplication {
 
 	public static void main(String[] args) {
