81b5a1d1daf1ec664da3e79aff730ebc648b00b3
chohw37 <chohw37@gmail.com>
2026-07-31 10:15:35 +0900

    bug fix

--- backend/src/main/java/kr/co/tobetheone/ncms/NcmsApplication.java
+++ backend/src/main/java/kr/co/tobetheone/ncms/NcmsApplication.java
@@ -1,5 +1,7 @@
 package kr.co.tobetheone.ncms;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
@@ -7,8 +9,14 @@ import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 @SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
 public class NcmsApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(NcmsApplication.class, args);
 	}
 
 }
+
